3b87bf1bd26b297e93dce979f4cac59281227a76
AryanAg08 <aryangoyal90@outlook.com>
2023-04-12 16:20:00 +0530

    V1.2.2

--- index.js
+++ index.js
@@ -55,8 +55,15 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'routes')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(cors({
     origin : ['https://jscop8-0.herokuapp.com/', "http://localhost:3000"],
+    
 }))
 app.use(cookieParser())
 
